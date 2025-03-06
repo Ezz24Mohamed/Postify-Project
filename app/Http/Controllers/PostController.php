e545942522850afd09dde4b06cea054ccc1915e1
zizoomohamed464@gmail.com <zizoomohamed464@gmail.com>
2025-03-06 22:42:06 +0200

    post controller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Posts;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Posts::where('user_id',Auth::id())->get();
+        return response()->json($posts,200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title'=>'required|string|max:255',
+            'content'=>'required|string'
+        ]);
+        $post=posts::create([
+            'user_id'=>Auth::id(),
+            'title'=>$request->title,
+            'content'=>$request->content,
+        ]);
+        return response()->json([
+            'message'=>'Post created successfully',
+            'post'=>$post,
+        ],201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Posts $post)
+    {
+        return response()->json($post, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Posts $post)
+    {
+      if($post->user_id!==Auth::id()){
+        return response()->json(['message'=>'User is not authorized'],403);
+
+      } 
+      $validated=$request->validate([
+        'title'=>'sometimes|string|max:255',
+        'content'=>'sometime|string',
+      ]); 
+      $post->update($validated);
+      return response()->json(['message'=>'Post updated successfully','post'=>$post],200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Posts $post)
+    {
+        if ($post->user_id !== Auth::id()) {
+            return response()->json(['message' => 'User is not authorized'], 403);
+        }
+
+        $post->delete();
+
+        return response()->json(['message' => 'Post deleted successfully'], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/posts', [PostController::class, 'index']);
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::get('/posts/{post}', [PostController::class, 'show']);
+    Route::put('/posts/{post}', [PostController::class, 'show']);
+    Route::delete('posts/{post}', [PostController::class, 'destroy']);
+
 });
